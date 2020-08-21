a0a3ea07618fd1b3ddf2b03a68753dcb6dbbb599
viralthaker15 <viralthaker27@gmail.com>
2020-08-21 19:56:22 +0530

    removed auto dispatch method from app.js

--- src/app.js
+++ src/app.js
@@ -21,22 +21,6 @@ import "./styles/styles.scss";
 
 const store = configureStore();
 
-store.dispatch(
-	addExpense({ description: "waterbill", amount: 100, createdAt: 2000 })
-);
-
-store.dispatch(
-	addExpense({ description: "rent", amount: 19000, createdAt: 23000 })
-);
-
-store.dispatch(
-	addExpense({ description: "gasbill", amount: 500, createdAt: 1000 })
-);
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
 	<Provider store={store}>
 		<AppRouter />
